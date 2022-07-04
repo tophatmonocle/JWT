22199443d399dd3c37e964e594de4953783846db
Blair McArthur <blair.mcarthur@tophatmonocle.com>
2022-07-04 16:14:25 -0400

    Switched to using an exact version of Base64 instead of a branch

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .package(url: "https://github.com/tophatmonocle/Base64", .branch("spm-support")),
+        .package(url: "https://github.com/tophatmonocle/Base64", .exact("1.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
